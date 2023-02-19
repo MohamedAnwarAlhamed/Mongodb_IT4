1e792f9ac556f75f1bdeccf00cb920f88066d382
Afnan-alghrbi <125822595+Afnan-alghrbi@users.noreply.github.com>
2023-02-19 22:54:30 +0300

    Update afnan.js

--- afnan.js
+++ afnan.js
@@ -1,28 +1,17 @@
-//1--Write a MongoDB query to find the restaurant name, borough, longitude and latitude and cuisine for those restaurants which 
-//contain 'Mad' as first three letters of its name.
-
+1
 
 db.restaurand.find({name:{$regex: /^Mad/i,}},{"name":1,"borough":1,"address.coord":1,"cuisine":1});
 
-//2-- Write a MongoDB query to find the restaurant name, borough, longitude and attitude and cuisine for those restaurants which contains 'mon' 
-//as three letters somewhere in its name
-
+2
 db.restaurand.find({name:{$regex: "mon.*"}},{"name":1,"borough":1,"address.coord":1,"cuisine":1});
 
-
-
-//3--Write a MongoDB query which will select the restaurant Id, name and grades for those restaurants which returns 0 as a remainder after dividing the score by 7. 
-
+3
 db.restaurand.find({"grades.score":{$mod: [7,0]}},{"restaurant_id":1,"name":1,"grades":1});
 
-
-//4--Write a MongoDB query which will select all documents in the restaurants collection where the coord field value is double.
-
+4
 db.restaurand.find({"address.coord":{$type: 1}});
 
-//5--Write a MongoDB query to find the restaurant Id, name, and grades for those restaurants where the 2nd element of grades array contains a grade of "A" and score 9 on an ISODate "2014-08-11T00:00:00Z"
-
-
+5
 db.restaurand.find(
     {
       "grades.1.grade":"A",
@@ -35,9 +24,7 @@ db.restaurand.find(
       "grades":1
     });
 
-//6--Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which prepared dish except 'American' and 'Chinees' or restaurant's name begins with letter 'Wil'.
-
-
+6
 db.restaurand.find(
     {
       $or: [ {name: /^Wil/},
@@ -56,10 +43,7 @@ db.restaurand.find(
       "cuisine":1
     });
 
-//7--Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which achieved a score which is not more than 10.
-
-
-
+7
 db.restaurand.find(
     {
       "grades.score":{$not:{$gt:10}}
@@ -70,9 +54,7 @@ db.restaurand.find(
       "cuisine":1,
       "grades.score":1
     });
-//8--Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which are not belonging to the borough Staten Island or Queens or Bronxor Brooklyn.
-
-
+8
 db.restaurand.find(
     {
       $and:[
@@ -89,9 +71,7 @@ db.restaurand.find(
       "cuisine":1,
     });
 
-//9-- Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which belong to the borough Staten Island or Queens or Bronxor Brooklyn. 
-
-
+9
 db.restaurand.find(
     {
       $or:[
@@ -108,8 +88,7 @@ db.restaurand.find(
       "cuisine":1,
     });
 
-//10--Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which belong to the borough Staten Island or Queens or Bronxor Brooklyn.
-
+10
 db.restaurand.find(
     {
       $or:[
@@ -127,8 +106,7 @@ db.restaurand.find(
     });
 
 
-//11--Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which contain 'Wil' as first three letters for its name
-
+11
 db.restaurand.find(
     {
       name: /^Wil/
@@ -140,9 +118,7 @@ db.restaurand.find(
       "cuisine":1
     });
 
-//12--Write a MongoDB query to find the restaurants which do not prepare any cuisine of 'American' and achieved a grade point 'A' not belongs to the borough Brooklyn.The document must be displayed according to the cuisine in descending order.
-
-
+12
 db.restaurand.find(
     {
       "cuisine":{$ne:"American"},
