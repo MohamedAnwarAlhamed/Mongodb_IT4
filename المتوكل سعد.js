28ccad591c4d2881a3402b7fef7b88862403dba6
Almttokl-salih <122641406+Almttokl-salih@users.noreply.github.com>
2023-02-20 12:37:21 -0700

    Add files via upload

---
+++ المتوكل سعد.js
@@ -0,0 +1,163 @@
+
+ 		
+
+// المتوكل سعد 
+
+//q- 1 
+
+db.restaurants.find(
+                   { name : 
+                     { $regex : /^Mad/i, } 
+                   },
+                       {
+                         "name":1,
+                         "borough":1,
+                         "address.coord":1,
+                         "cuisine" :1
+                        }
+                   );
+
+
+//q- 2 
+
+db.restaurants.find(
+                   { name : 
+                     { $regex : "mon.*", $options: "i" } 
+                   },
+                       {
+                         "name":1,
+                         "borough":1,
+                         "address.coord":1,
+                         "cuisine" :1
+                        }
+                   );
+				   
+				   
+//q- 3 
+
+
+db.restaurants.find(
+                      {"grades.score" :
+                         {$mod : [7,0]}
+                      },
+                         {"restaurant_id" : 1,"name":1,"grades":1}
+                    );
+
+
+//q- 4 
+
+
+db.restaurants.find(
+                    {"address.coord" : 
+                       {$type : 1}
+                    }
+                   );
+
+
+//q- 5 
+
+
+db.restaurants.find( 
+                      { "grades.1.date": ISODate("2014-08-11T00:00:00Z"), 
+                        "grades.1.grade":"A" , 
+                        "grades.1.score" : 9
+                      }, 
+                       {"restaurant_id" : 1,"name":1,"grades":1}
+                   );
+
+
+//q- 6
+
+
+db.restaurants.find(
+{$or: [
+  {name: /^Wil/}, 
+  {"$and": [
+       {"cuisine" : {$ne :"American "}}, 
+       {"cuisine" : {$ne :"Chinees"}}
+   ]}
+]}
+,{"restaurant_id" : 1,"name":1,"borough":1,"cuisine" :1}
+);
+
+
+//q- 7 
+
+
+db.restaurants.find(
+{"grades.score" : 
+{ $not: 
+{$gt : 10}
+}
+},
+{
+"restaurant_id" : 1,
+"name":1,"borough":1,
+"cuisine" :1
+}
+);
+
+
+//q- 8 
+
+
+db.restaurants.find(
+{"borough" :{$nin :["Staten Island","Queens","Bronx","Brooklyn"]}},
+{
+"restaurant_id" : 1,
+"name":1,"borough":1,
+"cuisine" :1
+}
+);
+
+
+//q- 9 
+
+db.restaurants.find(
+{"borough" :{$in :["Staten Island","Queens","Bronx","Brooklyn"]}},
+{
+"restaurant_id" : 1,
+"name":1,"borough":1,
+"cuisine" :1
+}
+);
+
+
+//q- 10
+
+
+db.restaurants.find(
+{"borough" :{$in :["Staten Island","Queens","Bronx","Brooklyn"]}},
+{
+"restaurant_id" : 1,
+"name":1,"borough":1,
+"cuisine" :1
+}
+);
+
+
+//q- 11 
+
+
+db.restaurants.find(
+{name: /^Wil/},
+{
+"restaurant_id" : 1,
+"name":1,"borough":1,
+"cuisine" :1
+}
+);
+
+
+//q- 12 
+
+db.restaurants.find( {
+                             "cuisine" : {$ne : "American"},
+                             "grades.grade" :"A",
+                             "borough": {$ne : "Brooklyn"}
+                       } 
+                    ).sort({"cuisine":-1});
+					
+					
+// *_* the end *_*
+					
